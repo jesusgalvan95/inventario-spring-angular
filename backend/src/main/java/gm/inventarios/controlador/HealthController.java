8879572ef7f534ed21f0363e4ded0501434e2031
jesusgalvan95 <jesusgalvangil95@gmail.com>
2026-02-08 14:21:49 +0100

    Add health endpoint for Render

---
+++ backend/src/main/java/gm/inventarios/controlador/HealthController.java
@@ -0,0 +1,14 @@
+package gm.inventarios.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+}
+
